12c0055386f8f3e6f3148613983d1eba943b1434
Parthib <sirajammunirparthi24@gmail.com>
2018-06-27 14:17:12 +0600

    Update ft5x0x_driver.c

--- drivers/input/touchscreen/mediatek/ft5x0x/ft5x0x_driver.c
+++ drivers/input/touchscreen/mediatek/ft5x0x/ft5x0x_driver.c
@@ -126,7 +126,7 @@ static struct device_attribute *ft5x0x_attrs[] = {
 };
 
 static struct tpd_driver_t tpd_device_driver = {
-	.tpd_device_name = "FT5x0x",
+	.tpd_device_name = "ft3427_zaw875",
 	.tpd_local_init = tpd_local_init,
 	.suspend = tpd_suspend,
 	.resume = tpd_resume,
